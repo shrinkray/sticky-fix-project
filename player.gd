7edaf0600fc0aa56f390d0ddc15980d98b1ec126
Greg Miller <hello@gregmiller.io>
2026-03-04 00:01:16 -0500

    feat: Initialize player and scene

--- player.gd
+++ player.gd
@@ -4,9 +4,9 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-    velocity.y += gravity * delta
-    move_and_slide()
+	velocity.y += gravity * delta
+	move_and_slide()
 
 func _input(event):
-    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-        velocity.y = JUMP_VELOCITY
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		velocity.y = JUMP_VELOCITY
